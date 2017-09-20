f6fdccb1f443ed0c31494a998484ac682976b090
superqwe <superqwe@users.noreply.github.com>
2017-09-21 00:21:50 +0200

    - Modificato ordine per inizio Poxwalkers (WH40K truppe) - 200/OR Officer, 95th Rifles, Waterloo 1815 (Mitches Military Models) stostat ad DA INIZIARE

--- miniature_dipinte/scaletta_miniature.py
+++ miniature_dipinte/scaletta_miniature.py
@@ -10,6 +10,7 @@ from miniature_dipinte.models import Classe_Modello, Esercito, Miniatura
 
 from miniature_dipinte.costanti import *
 
+# todo date delle miniature errate
 def elenco_truppe():
     whf = elenco_truppe2(L1, 'whft', OFFSET_WHF_TRUPPE)
     wh40k = elenco_truppe2(L2, 'wh40kt', OFFSET_WH40K_TRUPPE)
@@ -134,7 +135,7 @@ def elenco_elite():
 
             if i >= neserciti - 1:
                 break
-
+    # pp(miniature)
     return miniature
 
 
